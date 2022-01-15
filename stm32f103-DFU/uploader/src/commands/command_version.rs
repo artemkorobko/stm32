bba5756a0d52932e495e4aa8c4cf847f6273d609
artem <artem.korobko@gmail.com>
2022-01-15 16:35:57 +0100

    Rename version command

--- stm32f103-DFU/uploader/src/commands/command_version.rs
+++ stm32f103-DFU/uploader/src/commands/command_version.rs
@@ -2,15 +2,15 @@ use crate::driver::driver::Driver;
 
 use super::command_executor::CommandExecutor;
 
-pub struct CommandExecutorVersion;
+pub struct CommandVersion;
 
-impl CommandExecutorVersion {
+impl CommandVersion {
     pub fn boxed() -> Box<dyn CommandExecutor> {
         Box::new(Self {})
     }
 }
 
-impl CommandExecutor for CommandExecutorVersion {
+impl CommandExecutor for CommandVersion {
     fn exec(&self) -> anyhow::Result<()> {
         let ver = Driver::version();
         log::info!("Driver version: {}.{}.{}", ver.major, ver.minor, ver.patch);
