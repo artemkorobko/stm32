0d60308659260124168e865cb548138b5ba0dee7
artem <artem.korobko@gmail.com>
2022-01-15 16:35:05 +0100

    Add defaule flags command

---
+++ stm32f103-DFU/uploader/src/commands/command_flags.rs
@@ -0,0 +1,23 @@
+use crate::driver::driver::Driver;
+
+use super::command_executor::CommandExecutor;
+
+pub struct CommandFlags {
+    driver: Driver,
+}
+
+impl CommandFlags {
+    pub fn new(driver: Driver) -> Self {
+        Self { driver }
+    }
+
+    pub fn boxed(self) -> Box<dyn CommandExecutor> {
+        Box::new(self)
+    }
+}
+
+impl CommandExecutor for CommandFlags {
+    fn exec(&self) -> anyhow::Result<()> {
+        todo!()
+    }
+}
