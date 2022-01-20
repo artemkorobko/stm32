4259ba6e7a46765cf1428de2674ffb4856434bd0
artem <artem.korobko@gmail.com>
2022-01-20 22:34:44 +0100

    Add protocol validation methods

--- stm32f103-DFU/uploader/src/driver/protocol/common.rs
+++ stm32f103-DFU/uploader/src/driver/protocol/common.rs
@@ -21,9 +21,12 @@ pub trait CommonProtocol {
 impl CommonProtocol for OpenedDevice {
     fn firmware_version(&mut self) -> anyhow::Result<FirmwareVersion> {
         let buf = [0];
-        self.write_all(&buf)?;
+        let size = self.write_all(&buf)?;
+        validate_size_sent(size, 1)?;
         let mut buf = [0; 64];
-        self.read(&mut buf, DEFAULT_IO_TIMEOUT)?;
+        let size = self.read(&mut buf, DEFAULT_IO_TIMEOUT)?;
+        validate_size_received(size, 4)?;
+        validate_opcode(buf[0], 0)?;
         Ok(FirmwareVersion {
             major: buf[1],
             minor: buf[2],
@@ -33,9 +36,12 @@ impl CommonProtocol for OpenedDevice {
 
     fn device_id(&mut self) -> anyhow::Result<DeviceId> {
         let buf = [1];
-        self.write_all(&buf)?;
+        let size = self.write_all(&buf)?;
+        validate_size_sent(size, 1)?;
         let mut buf = [0; 64];
-        self.read(&mut buf, DEFAULT_IO_TIMEOUT)?;
+        let size = self.read(&mut buf, DEFAULT_IO_TIMEOUT)?;
+        validate_size_received(size, 13)?;
+        validate_opcode(buf[0], 1)?;
         Ok(DeviceId {
             id_0: buf[1] as u16 | ((buf[2] as u16) << 8),
             id_1: buf[3] as u16 | ((buf[4] as u16) << 8),
@@ -48,5 +54,41 @@ impl CommonProtocol for OpenedDevice {
                 | ((buf[11] as u32) << 16)
                 | ((buf[12] as u32) << 24),
         })
+    }
+}
+
+pub fn validate_size_sent(sent: usize, expected: usize) -> anyhow::Result<()> {
+    if sent == expected {
+        Ok(())
+    } else {
+        anyhow::bail!(
+            "Can't send {} bytes to USB device, sent: {}",
+            expected,
+            sent,
+        )
+    }
+}
+
+pub fn validate_size_received(received: usize, expected: usize) -> anyhow::Result<()> {
+    if received == expected {
+        Ok(())
+    } else {
+        anyhow::bail!(
+            "Can't receive {} bytes from USB device, received: {}",
+            expected,
+            received,
+        )
+    }
+}
+
+pub fn validate_opcode(received: u8, expected: u8) -> anyhow::Result<()> {
+    if received == expected {
+        Ok(())
+    } else {
+        anyhow::bail!(
+            "Invalid opcode {} received from USB device, expected: {}",
+            received,
+            expected,
+        )
     }
 }
